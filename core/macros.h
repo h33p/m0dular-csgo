b715887a69d083819c1d9d36c01145297a9a62f7
Aurimas Blažulionis <0x60@pm.me>
2018-06-26 21:10:24 +0300

    Add a macros file.

---
+++ core/macros.h
@@ -0,0 +1,12 @@
+#ifndef CSGO_MACROS_H
+#define CSGO_MACROS_H
+
+#ifdef DEBUG
+#define DEVMSG(...) cvar->ConsoleDPrintf(__VA_ARGS__)
+#define CDEVMSG(c, ...) auto col = c; cvar->ConsoleColorPrintf((uint8_t*)&col, __VA_ARGS__)
+#else
+#define DEVMSG(...)
+#define CDEVMSG(...)
+#endif
+
+#endif
